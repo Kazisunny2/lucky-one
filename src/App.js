f3461694d0e129775485a1f744ad3bba80effbb1
Kazisunny2 <saidul.bit@gmail.com>
2022-03-26 03:58:00 +0100

    sixth Commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
    <Shop></Shop>
    <h2>How React Works?</h2>
    <p>React is a JavaScript library (not a framework) that creates user interfaces (UIs) in a predictable and efficient way using declarative code. We can use it to help build single page applications and mobile apps, or to build complex apps if utilise it with other libraries.</p>
+   <br />
+   <h2>How useState works?</h2>
+   <p>useState enables to add state to function components. Calling React.useState inside a function component generates a single piece of state associated with that component. Whereas the state in a class is always an object, with Hooks, the state can be any type. Each piece of state holds a single value, which can be an object, an array, a boolean, or any other type you can imagine.</p>
     </div>
   );
 }
